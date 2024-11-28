ef867638b75f99d61b73dee6ba0a825a6ad313a3
alexk <alex.krotov3333@gmail.com>
2024-11-28 11:53:44 +0300

    Добавил модели.

---
+++ internal/model/brands.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+// Brand представляет сущность бренда
+type Brand struct {
+	ID            int64     `json:"id"`              // BIGSERIAL
+	Name          string    `json:"name"`            // NOT NULL
+	Link          *string   `json:"link"`            // Может быть NULL
+	Description   *string   `json:"description"`     // Описание/история бренда
+	LogoURL       *string   `json:"logo_url"`        // URL логотипа
+	CoverImageURL *string   `json:"cover_image_url"` // URL обложки
+	FoundedYear   *int      `json:"founded_year"`    // Год основания
+	OriginCountry *string   `json:"origin_country"`  // Страна происхождения
+	Popularity    int       `json:"popularity"`      // Индекс популярности
+	IsPremium     bool      `json:"is_premium"`      // Флаг премиального бренда
+	IsUpcoming    bool      `json:"is_upcoming"`     // Флаг "Скоро"
+	CreatedAt     time.Time `json:"created_at"`      // Время создания
+	UpdatedAt     time.Time `json:"updated_at"`      // Время обновления
+}
---
+++ internal/model/model.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type Model struct {
+	ID          int64      `json:"id"`           // BIGSERIAL
+	BrandID     int64      `json:"brand_id"`     // Ссылка на бренд
+	Name        string     `json:"name"`         // Название модели
+	ReleaseDate *time.Time `json:"release_date"` // Дата релиза
+	IsUpcoming  bool       `json:"is_upcoming"`  // Флаг "Скоро"
+	IsLimited   bool       `json:"is_limited"`   // Флаг ограниченного выпуска
+	CreatedAt   time.Time  `json:"created_at"`   // Время создания
+	UpdatedAt   time.Time  `json:"updated_at"`   // Время обновления
+}
